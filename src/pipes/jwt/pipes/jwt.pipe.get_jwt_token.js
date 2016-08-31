78d58e7c32033b97d8d9f59284b1da1625e01534
origamyllc <prashunjaveri@gmail.com>
2016-08-30 23:51:08 -0700

    changing auth to use pipeline

--- src/pipes/jwt/pipes/jwt.pipe.get_jwt_token.js
+++ src/pipes/jwt/pipes/jwt.pipe.get_jwt_token.js
@@ -14,23 +14,24 @@ export function get_jwt_token(req,res){
 const initialize_pipe = function (req,res) {
     return   new Promise(() => {
         req.log.info("getting jwt token by access token");
-        get_jwt_token_by_access_token( req, res);
+        const access_key = req.headers.authorization ;
+        get_jwt_token_by_access_token(  access_key ).then( (jwt_token) => {
+                 res.setHeader("authorization",  jwt_token.response.value);
+                 responses.sendSuccessResponse(res, {"message": "authorized"});
+        }).catch( () => {
+             req.log.error("can not get JWT token ")
+             responses.send_unauthorized_user_error(req, res);
+        });
     });
 }
 
-function  get_jwt_token_by_access_token(req, res ){
-    const access_key = req.headers.authorization ;
-    orchestrator_fascade.get_jwt_token_by_access_token(access_key).then((jwt_token) => {
-        if(jwt_token.status !== 200){
-            req.log.error( "can not get jwt token by access token ");
-            responses.send_unauthorized_user_error(req, res);
-        }
-        else {
-            res.setHeader("authorization",  jwt_token.response.value);
-            responses.sendSuccessResponse(res, {"message": "authorized"});
-        }
-    }).catch(() => {
-        req.log.error("can not get JWT token ")
-        responses.send_unauthorized_user_error(req, res);
+function  get_jwt_token_by_access_token( access_key  ){
+    return   new Promise((resolve) => {
+        var error = { error : "can not get jwt token by access token " }
+        orchestrator_fascade.get_jwt_token_by_access_token(access_key).then((jwt_token) => {
+                resolve(jwt_token);
+        }).catch(() => {
+            resolve(error);
+        });
     });
 }
