5c3268d6d668d48f3eacecbb7e156a828831a599
origamyllc <prashunjaveri@gmail.com>
2016-08-30 23:27:05 -0700

    changing auth to use pipeline

--- src/pipes/auth/pipes/auth.pipe.get_access_token_by_user_info.js
+++ src/pipes/auth/pipes/auth.pipe.get_access_token_by_user_info.js
@@ -8,7 +8,7 @@ export function get_access_token_by_user(req, res) {
 
 const initialize_pipe = function (req,res) {
     login_user (req)
-        .then(get_user_by_name)
+        .then(get_user_id_by_name)
         .then(get_user_token_by_id)
         .then((token) => {
              responses.send_response(res, {"accesstoken": token.docs[0].accessToken })
@@ -29,7 +29,7 @@ function login_user (req){
     });
 }
 
-const get_user_by_name =  function (user) {
+const get_user_id_by_name =  function (user) {
    var user_name = JSON.parse(user)["username"];
     return new Promise((resolve) => {
         authentication_fascade.get_user_by_name(user_name).then((user) => {
